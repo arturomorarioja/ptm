c1c60e999009a5b1b342c0457740438bfd901542
Arturo Mora-Rioja <arturomora@gmail.com>
2018-04-09 00:24:35 +0200

    PHP code embellished

--- src/classes.php
+++ src/classes.php
@@ -1,15 +1,20 @@
 <?php
     include("connection.php");
 
-    /*
-        Encapsulates a customer. Takes care of the corresponding queries, inserts, updates, and deletes
-
-        Author: Arturo Mora-Rioja
-        Date: 6/4/2018
-    */
+    /**
+     * Encapsulates the customers. 
+     * Takes care of the corresponding queries, inserts, updates, and deletes
+     *
+     * @author Arturo Mora-Rioja
+     * @date   6/4/2018
+     */
     class Customer extends DB {
 
-        // Returns a bidimensional array with the data of all customers
+        /**
+         * Gets the data of all customers
+         * 
+         * @return bidimensional array [customer row, column values] / false if there was any error 
+         */
         function aAllCustomers(){
             $cnDB = $this->dbConnect();
             if(!$cnDB)
@@ -31,7 +36,12 @@
             }
         }
 
-        // Returns an array with the data of the customer whose PK receives as a parameter
+        /**
+         * Gets the data of a customer
+         * 
+         * @param  customer id
+         * @return array [customer column values] / false if there was any error
+         */
         function aCustomer($pnCustomerID){
             $cnDB = $this->dbConnect();
             if(!$cnDB)
@@ -51,7 +61,19 @@
             }
         }
 
-        // Inserts a new customer
+        /**
+         * Inserts a new customer
+         * 
+         * @param  customer CVR
+         * @param  customer name
+         * @param  customer address
+         * @param  customer postal code
+         * @param  customer town
+         * @param  customer phone number
+         * @param  customer e-mail
+         * @param  customer country id
+         * @return true if the insertion was correct, false if there was an error
+         */
         function bInsert($pcCVR, $pcName, $pcAddress, $pcPostCode, $pcTown, $pcPhone, $pcEmail, $pcCountryID){
             $cnDB = $this->dbConnect();
             if(!$cnDB)
@@ -75,8 +97,22 @@
             }
         }
         
-        // Updates all values of the customer whose PK receives as first parameter
-        function bUpdate($pnCustomerID, $pcCVR, $pcName, $pcAddress, $pcPostCode, $pcTown, $pcPhone, $pcEmail, $pcCountryID){
+        /**
+         * Updates all the values of a customer
+         * 
+         * @param  id of the customer whose data will be updated
+         * @param  new customer CVR
+         * @param  new customer name
+         * @param  new customer address
+         * @param  new customer postal code
+         * @param  new customer town
+         * @param  new customer phone number
+         * @param  new customer e-mail
+         * @param  new id of the customer's country 
+         * @return true if the update was correct, false if there was an error
+         */
+        function bUpdate($pnCustomerID, $pcCVR, $pcName, $pcAddress, $pcPostCode, $pcTown, $pcPhone, 
+                $pcEmail, $pcCountryID){
             $cnDB = $this->dbConnect();
             if(!$cnDB)
                 return false;
@@ -98,9 +134,13 @@
                 return($bOk);
             }
         }
-
-        // Deletes the customer whose PK receives as a parameter. Return values:
-        // 0 Error / 1 Success / 2 Referential integrity problem
+        
+        /**
+         * Deletes a customer
+         * 
+         * @param  id of the customer to delete
+         * @return 0 error / 1 success / 2 referential integrity problem
+         */
         function nDelete($pnCustomerID){
             define("RET_ERROR", 0);
             define("RET_OK", 1);
@@ -132,14 +172,19 @@
         }
     }
 
-    /*
-        Encapsulates the countries. Allows their querying
-
-        Author: Arturo Mora-Rioja
-        Date: 7/4/2018
-    */
+    /**
+     * Encapsulates the countries. Allows their being queried
+     * 
+     * @author Arturo Mora-Rioja
+     * @date   7/4/2018
+     */
     class Country extends DB {
-        // Returns a bidimensional array with the data of all countries
+
+        /**
+         * Gets the data of all the countries
+         * 
+         * @return bidimensional array [country row, column values] / false if there was any error
+         */
         function aAllCountries(){
             $cnDB = $this->dbConnect();
             if(!$cnDB)
@@ -162,14 +207,19 @@
         }        
     }
 
-    /*
-        Encapsulates the projects
-
-        Author: Arturo Mora-Rioja
-        Date: 7/4/2018
-    */
+    /**
+     * Encapsulates the projects. 
+     * Takes care of the corresponding queries, inserts, updates, and deletes
+     *
+     * @author Arturo Mora-Rioja
+     * @date   7/4/2018
+     */
     class Project extends DB {
-        // Returns a bidimensional array with the data of all customers
+        /**
+         * Gets the data of all projects
+         * 
+         * @return bidimensional array [project row, column values] / false if there was any error 
+         */
         function aAllProjects(){
             $cnDB = $this->dbConnect();
             if(!$cnDB)
@@ -191,7 +241,12 @@
             }
         }      
 
-        // Returns an array with the projects of the customer whose PK receives as a parameter
+        /**
+         * Gets the projects of a customer
+         * 
+         * @param  id of the customer whose projects are to be retrieved
+         * @return bidimensional array [project row, id and name] / false if there was any error
+         */
         function aProjectsByCustomer($pnCustomerID){
             $cnDB = $this->dbConnect();
             if(!$cnDB)
@@ -214,7 +269,12 @@
             }
         }      
 
-        // Returns an array with the data of the project whose PK receives as a parameter
+        /**
+         * Gets the data of a project
+         * 
+         * @param  project id
+         * @return array [project column values] / false if there was any error
+         */
         function aProject($pnProjectID){
             $cnDB = $this->dbConnect();
             if(!$cnDB)
@@ -234,7 +294,16 @@
             }
         }
 
-        // Inserts a new project
+        /**
+         * Inserts a new project
+         * 
+         * @param  id of the project's customer
+         * @param  project name
+         * @param  project start date
+         * @param  project end date
+         * @param  project comments
+         * @return true if the insertion was correct, false if there was an error
+         */
         function bInsert($pnCustomerID, $pcName, $pdStart, $pdEnd, $pcComments){
             $cnDB = $this->dbConnect();
             if(!$cnDB)
@@ -255,7 +324,17 @@
             }
         }
         
-        // Updates all values of the project whose PK receives as first parameter
+        /**
+         * Updates all the values of a project
+         * 
+         * @param  id of the project whose data will be updated
+         * @param  new id of the project's customer
+         * @param  new project name
+         * @param  new project start date
+         * @param  new project end date
+         * @param  new project comments
+         * @return true if the update was correct, false if there was an error
+         */
         function bUpdate($pnProjectID, $pnCustomerID, $pcName, $pdStart, $pdEnd, $pcComments){
             $cnDB = $this->dbConnect();
             if(!$cnDB)
@@ -276,8 +355,12 @@
             }
         }
 
-        // Deletes the project whose PK receives as a parameter. Return values:
-        // 0 Error / 1 Success / 2 Referential integrity problem
+        /**
+         * Deletes a project
+         * 
+         * @param  id of the project to delete
+         * @return 0 error / 1 success / 2 referential integrity problem
+         */
         function nDelete($pnProjectID){
             define("RET_ERROR", 0);
             define("RET_OK", 1);
@@ -309,16 +392,22 @@
         }        
     }
 
-    /*
-        Encapsulates the registered times
-
-        Author: Arturo Mora-Rioja
-        Date: 7/4/2018
-    */
+    /**
+     * Encapsulates the registered times. 
+     * Takes care of the corresponding queries, inserts, updates, and deletes
+     *
+     * @author Arturo Mora-Rioja
+     * @date   7/4/2018
+     */
     class RegTime extends DB {
-        // Returns a bidimensional array with the data of all registered times of the customer or project
-        // whose PK it receives as first parameter. 
-        // The second parameter specifies whether the first parameter refers to a customer (true) or project (false)
+    
+        /**
+         * Gets the registered times of a customer or project
+         * 
+         * @param  customer id or project id (see below) 
+         * @param  if true, $pnValueID is a customer id / if false, $pnValueID is a project id
+         * @return bidimensional array [registered time row, column values] / false if there was any error 
+         */
         function aRegTimes($pnValueID, $pbIsCustomer){
             $cnDB = $this->dbConnect();
             if(!$cnDB)
@@ -345,7 +434,12 @@
             }
         }
         
-        // Returns an array with the data of the registered time whose PK receives as a parameter
+        /**
+         * Gets the data of a registered time
+         * 
+         * @param  registered time id
+         * @return array [registered time column values] / false if there was any error
+         */
         function aRegTime($pnRegTimeID){
             $cnDB = $this->dbConnect();
             if(!$cnDB)
@@ -367,7 +461,15 @@
             }
         }
 
-        // Inserts a new registered time
+        /**
+         * Inserts a new registered time
+         * 
+         * @param  id of the registered time's project
+         * @param  registration date
+         * @param  registered time
+         * @param  registered time comments
+         * @return true if the insertion was correct, false if there was an error
+         */
         function bInsert($pnProjectID, $pdRegistration, $pdRegTime, $pcComments){
             $cnDB = $this->dbConnect();
             if(!$cnDB)
@@ -387,7 +489,16 @@
             }
         }
 
-        // Updates all values of the registered time whose PK receives as first parameter
+        /**
+         * Updates all the values of a registered time
+         * 
+         * @param  id of the registered time whose data will be updated
+         * @param  new project id of the registered time
+         * @param  new registration date
+         * @param  new registered time
+         * @param  new registered time comment
+         * @return true if the update was correct, false if there was an error
+         */
         function bUpdate($pnRegTimeID, $pnProjectID, $pdRegDate, $pdTime, $pcComments){
             $cnDB = $this->dbConnect();
             if(!$cnDB)
@@ -407,8 +518,12 @@
             }
         }
 
-        // Deletes the registered time whose PK receives as a parameter. Return values:
-        // 0 Error / 1 Success 
+        /**
+         * Deletes a registered time
+         * 
+         * @param  id of the registered time to delete
+         * @return 0 error / 1 success
+         */
         function nDelete($pnRegTimeID){
             define("RET_ERROR", 0);
             define("RET_OK", 1);
